62220ec4480832b7b4ec5a56081e236a747bad3a
Park Jun Kyung <wnsrud6839@gmail.com>
2021-04-21 11:25:03 +0900

    make up vue file

--- js-draggable/vue-draggable-resizable-example/stories/how-to/index.js
+++
@@ -1,6 +0,0 @@
-import DragMultiple from './drag-multiple'
-import DragMultipleNotes from './notes/drag-multiple.md'
-
-export default {
-  'Drag Multiple Elements':  [() => new DragMultiple, DragMultipleNotes]
-}
